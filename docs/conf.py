02bf0573b44821b4d436071a3af992b2a6d65cf8
Anthony Meza <64243783+anthony-meza@users.noreply.github.com>
2026-06-18 12:24:40 -0400

    adding html

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@ master_doc = "index"
 exclude_patterns = ["_build"]
 html_theme = "sphinx_rtd_theme"
 nb_execution_mode = "force"
+nb_execution_startup_code = "import xarray as xr\nxr.set_options(display_style='html')"
 
 
 HERE = Path(__file__).resolve()
